16a5342d3d229156e17ba3d4c03c5814a1fceba0
Pooria Dabbaghi <dpooria75@gmail.com>
2023-01-18 17:35:41 +0100

    final pi_mpi

--- Week3/ExcerciseDay13/pi_mpi.f90
+++ Week3/ExcerciseDay13/pi_mpi.f90
@@ -2,50 +2,51 @@
          USE MPI
          IMPLICIT NONE
          INTEGER, PARAMETER :: MK = kind(1.0D0)
-         INTEGER :: l_circle=0, l_square=0, g_circle=0, g_square=0, i, N=INT(1E8), M, n_pp
+         INTEGER :: l_circle=0, g_circle=0, g_square=0, i, N=INT(1E8), M, n_pp
          REAL(MK) :: pi
+         REAL(MK) :: tw1, tw2
          REAL(MK), DIMENSION(2) :: rand
          INTEGER, ALLOCATABLE, DIMENSION(:) :: seed
-         INTEGER :: tw1, tw2
          INTEGER :: p_rank, c_size, ierror, m_size
 
          CALL MPI_INIT(ierror)
          CALL MPI_COMM_RANK(MPI_COMM_WORLD, p_rank, ierror)
          CALL MPI_COMM_SIZE(MPI_COMM_WORLD, c_size, ierror)
-
+         l_circle = 0
+         g_circle = 0
          n_pp = N/c_size
-         M = n_pp * c_size
+         ! M = n_pp * c_size
 
-         CALL RANDOM_SEED(SIZE=seedsize)
-         ALLOCATE(seed(seedsize))
-         DO i=1, seedsize
-            seed(i) = 1238239 + p_rank*8365
-         ENDDO
 
-         CALL RANDOM_SEED(PUT=seed)
+         ! ! CALL RANDOM_SEED(SIZE=seedsize)
+         ! ! ALLOCATE(seed(seedsize))
+         ! ! DO i=1, seedsize
+         ! !    seed(i) = 1238239 + p_rank*8365
+         ! ! ENDDO
+
+         ! CALL RANDOM_SEED(PUT=seed)
 
          tw1 = MPI_WTIME()
 
          DO i=1, n_pp
             CALL RANDOM_NUMBER(rand)
-            IF (NORM2(rand) .LE. 1.0_MK) then
+            IF ((rand(1)**2 + rand(2)**2) .LE. 1.0_MK) then
                l_circle = l_circle + 1
-            ELSE
-               l_square = l_square +1
             ENDIF
          ENDDO
 
-         CALL MPI_REDUCE(l_circle, g_circle, 1, MPI_DOUBLE_PRECISION, MPI_SUM, 0, MPI_COMM_WORLD, ierror)
+         CALL MPI_REDUCE(l_circle, g_circle, 1, MPI_INTEGER, MPI_SUM, 0, MPI_COMM_WORLD, ierror)
          tw2 = MPI_WTIME()
 
          IF (p_rank .EQ. 0) then
-            pi = 4.0_mk * g_circle / (M + 0.0_MK)
+            PRINT*, c_size, l_circle, g_circle
+            pi = 4.0_mk * g_circle / (n_pp * c_size)
             PRINT *, 'pi =', pi
             PRINT*, 'WALL time is:', tw2-tw1
          ENDIF
 
          CALL MPI_FINALIZE(ierror)
-
+      
       END PROGRAM pi_mpi
 
 
---
+++ Week3/ExcerciseDay13/sub.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#BSUB -J pi
+#BSUB -o pi_%J.out
+#BSUB -q hpc
+#BSUB -n 24 # specify number of cores
+#BSUB -W 10:00 # specify maximum wall clock time
+#BSUB -u s000000@student.dtu.dk
+#BSUB -R "span[hosts=1]" # reserve all cores on one node
+##BSUB -R "span[ptile=1]" # reserve one core per node
+#BSUB -R "rusage[mem=16GB]"
+#BSUB -B # send notification at start
+#BSUB -N # send notification at end
+
+
+module add studio
+module add mpi/3.1.3-oracle-12u6
+
+mpif90 -free -O3 pi_mpi.f90 -o pi_mpi
+mpirun -n 24 ./pi_mpi
