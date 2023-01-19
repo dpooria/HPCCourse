06c40b22b80ae93c3690678eed84224c4f285965
Pooria Dabbaghi <dpooria75@gmail.com>
2023-01-19 10:50:58 +0100

    pi mpi

--- Week3/ExcerciseDay13/pi_mpi.f90
+++ Week3/ExcerciseDay13/pi_mpi.f90
@@ -3,28 +3,26 @@ PROGRAM pi_mpi
    IMPLICIT NONE
    INTEGER, PARAMETER :: MK = kind(1.0D0)
    INTEGER :: l_circle=0, g_circle=0, g_square=0, i, N
-   REAL(MK) :: pi, pi_old, eps = 1e-3
+   REAL(MK) :: pi, pi_old, eps = 1e-8
    REAL(MK) :: tw1, tw2
    REAL(MK), DIMENSION(2) :: rand
    INTEGER, ALLOCATABLE, DIMENSION(:) :: seed
-   INTEGER :: p_rank, c_size, ierror, m_size, req
-   LOGICAL:: notconverged = .TRUE., notconverged_root = .TRUE.
+   INTEGER :: p_rank, c_size, ierror, m_size, req, seedsize, t
+   LOGICAL:: converged = .FALSE.
    CALL MPI_INIT(ierror)
    CALL MPI_COMM_RANK(MPI_COMM_WORLD, p_rank, ierror)
    CALL MPI_COMM_SIZE(MPI_COMM_WORLD, c_size, ierror)
    l_circle = 0
    g_circle = 0
    N = 0
-   ! M = n_pp * c_size
+   CALL SYSTEM_CLOCK(t)
+   CALL RANDOM_SEED(SIZE=seedsize)
+   ALLOCATE(seed(seedsize))
+   DO i=1, seedsize
+      seed(i) = t + p_rank*123
+   ENDDO
 
-
-   ! ! CALL RANDOM_SEED(SIZE=seedsize)
-   ! ! ALLOCATE(seed(seedsize))
-   ! ! DO i=1, seedsize
-   ! !    seed(i) = 1238239 + p_rank*8365
-   ! ! ENDDO
-
-   ! CALL RANDOM_SEED(PUT=seed)
+   CALL RANDOM_SEED(PUT=seed)
 
    tw1 = MPI_WTIME()
    i = 0
@@ -41,21 +39,17 @@ PROGRAM pi_mpi
             pi_old = pi
             pi = 4.0_mk * g_circle / REAL(N, kind=MK)
             IF(abs(pi - pi_old) .LE. eps) THEN
-               notconverged_root = .FALSE.
-               notconverged = .FALSE.
+               converged = .TRUE.
             ENDIF
-            print*, 'so far so good', pi, N
          ENDIF
-         CALL MPI_SCATTER(notconverged_root, 1, MPI_LOGICAL, &
-            notconverged, 1, MPI_LOGICAL, 0, MPI_COMM_WORLD, ierror)
-         print*, "scatterrrr"
-         IF(.NOT. notconverged) EXIT
+         CALL MPI_Bcast(converged, 1, MPI_LOGICAL, 0, MPI_COMM_WORLD, ierror)
+         IF (converged) EXIT
       ENDIF
    ENDDO
    tw2 = MPI_WTIME()
    IF (p_rank .EQ. 0) THEN
       OPEN(10, file="pi_mpi_res.dat", action="write", position="append")
-      WRITE(10, *) pi, c_size, tw2 - tw1
+      WRITE(10, *) pi, c_size, tw2 - tw1, N
    ENDIF
    CALL MPI_FINALIZE(ierror)
 
